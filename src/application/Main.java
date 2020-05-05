018d04346d4f2ac45288bc5ae26e8ab312ad7fe6
Cindy Okino <cindy.okino@gmail.com>
2020-05-05 19:57:28 -0400

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -19,13 +21,17 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
-			primaryStage.setScene(mainScene);
+			mainScene = new Scene(scrollPane); //cria nova cena
+			primaryStage.setScene(mainScene); //coloca nova cena no palco
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() { //pega o mainScene criado no inicio do codigo como atributo privado
+		return mainScene; 
 	}
 
 	public static void main(String[] args) {
